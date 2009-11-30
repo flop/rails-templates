e21b43201b476407752fd3b40c3b195afe6c1161
flo <flo@yume.(none)>
2009-11-30 18:15:37 +0100

    update skeleton

--- skeleton.rb
+++ skeleton.rb
@@ -5,5 +5,5 @@ user = ask("What's your user to sylogix svn ?")
 password = ask("What's your password to sylogix svn ?")
 puts "Installing plugins..."
 plugin 'engines', :git => 'git://github.com/lazyatom/engines.git'
-plugin 'georges', :svn => "http://#{user}:#{password}@svn.sylogix.org/georges/trunk/georges_the_3rd"
+run "svn co http://#{user}:#{password}@svn.sylogix.org/georges/trunk/georges_the_3rd vendor/plugins/georges"
  
